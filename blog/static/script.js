06aaf0c526f14d19b36700b0e7e2b9894177340a
JMieding <jamesmieding@Guest.local>
2015-12-16 20:48:19 -0600

    Add misc small tweaks to style/layout/format. Think margin adjustments, tinkering with link color, etc. Most important: Navbar tabs now active when on that page (script.js) and font combination for the site is montserrat/nueton.

--- blog/static/script.js
+++ blog/static/script.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
 	
-	$("nav").hover(
-		function() {
-			$(this).fadeTo(650,1);
-		},function() {
-			$(this).fadeTo(650,0);
-		
-	});
+  var url = window.location.href;
+  var articles = document.getElementById('articles');
+
+  if (url.indexOf('articles') !== -1) {
+    $(articles).addClass('active');
+  } else {
+    $('.navbar-nav a').each(function() {
+      if (url == (this.href)) {
+        $(this).closest('li').addClass('active');
+      }
+    });
+  };
 });
+
+  // $(".social-icons").hover(function() {
+  //   $(this).animate({width: '2em'});
+  // },
+  // function(){
+  //   $(this).animate({width: '2.25em'});
+  // });
