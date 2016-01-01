e56830e0478341d1ccddfe7e8d86880d3b9152b2
JMieding <jamesmieding@Guest.local>
2015-12-31 20:26:00 -0600

    Add Comment model to Models.py, and migrate database

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -4,37 +4,41 @@ from django.template.defaultfilters import slugify
 from tinymce.models import HTMLField
 
 class Post(models.Model):
-	title = models.CharField(max_length=400)
-	text = HTMLField()
-	published_date = models.DateTimeField()
-	categories = models.CharField(max_length=100, default='')
-	slug = models.SlugField(max_length=400, unique=False, default='')
+  title = models.CharField(max_length=400)
+  text = HTMLField()
+  published_date = models.DateTimeField()
+  categories = models.CharField(max_length=100, default='')
+  slug = models.SlugField(max_length=400, unique=False, default='')
 
-	def publish(self):
-		if not self.id:
-			self.slug = slugify(self.title)
-			self.published_date = timezone.now()
-		self.save()
+  def publish(self):
+    if not self.id:
+      self.slug = slugify(self.title)
+      self.published_date = timezone.now()
+    self.save()
 
-	def __unicode__(self):
-		return self.title
+  def __unicode__(self):
+    return self.title
 
-# class Comment(models.Model):
-# 	text = models.CharField(max_length=1000)
-# 	post = models.ForeignKey(Post)
-# 	date = models.DateTimeField(auto_now_add=True)
+class Comment(models.Model):
+  text = models.CharField(max_length=400)
+  author = models.CharField(max_length=40)
+  date = models.DateTimeField(auto_now_add=True)
+  post = models.ForeignKey(Post)
 
-# 	def __unicode__(self):
-# 		return self.text
+  def publish(self):
+    self.save()
+
+  def __unicode__(self):
+    return self.text
 
 # Create your models here.
 # sample model syntax:
 
 #class ModelName(models.Model):
-# 	email = models.EmailField()
-# 	full_name = models.CharField(max_length=120, blank = True, null=True)
-# 	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)
-# 	updated = models.DateTimeField(auto_now_add = False, auto_now = True)
+#   email = models.EmailField()
+#   full_name = models.CharField(max_length=120, blank = True, null=True)
+#   timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)
+#   updated = models.DateTimeField(auto_now_add = False, auto_now = True)
 
-# 	def __unicode__(self): # in Python 3.3, this is __str__
-# 		return self.email
+#   def __unicode__(self): # in Python 3.3, this is __str__
+#     return self.email
