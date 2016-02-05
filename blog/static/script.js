c7884ed9ed731eb59324aabaac6507095b09eb66
JMieding <jtmieding@gmail.com>
2016-02-05 12:56:03 -0600

    From Static directory: 1) delete flaticonpack, 2) add images for blog posts, 3) update main css and javascript files

--- blog/static/script.js
+++ blog/static/script.js
@@ -1,22 +1,15 @@
 $(document).ready(function() {
-	
+	"use strict";
   var url = window.location.href;
   var articles = document.getElementById('articles');
 
   if (url.indexOf('articles') !== -1) {
-    $(articles).addClass('active');
+    $(articles).addClass('tab-nav-button');
   } else {
     $('.navbar-nav a').each(function() {
       if (url == (this.href)) {
-        $(this).closest('li').addClass('active');
+        $(this).addClass('tab-nav-button');
       }
     });
   };
 });
-
-  // $(".social-icons").hover(function() {
-  //   $(this).animate({width: '2em'});
-  // },
-  // function(){
-  //   $(this).animate({width: '2.25em'});
-  // });
